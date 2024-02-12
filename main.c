823f38ae2aa291dc8c4e85ba02c7ee3ca0ae10db
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-02-12 10:57:11 -0700

    initialized with given code

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+static char *randstring(size_t length) {
+  static char charset[] =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*"
+      "()_+-=[]{}\\|;:,<.>/?";
+  char *randomString = NULL;
+
+  if (!length) return NULL;
+  if ((randomString = malloc(sizeof(char) * (length + 1))) == NULL) return NULL;
+
+  for (size_t i = 0; i < length; i++) {
+    int key = rand() % (int)(sizeof(charset) - 1);
+    randomString[i] = charset[key];
+  }
+
+  randomString[length] = '\0';
+
+  return randomString;
+}
+
+int main(void) {
+  char *s;
+  size_t len = 5;
+
+  s = randstring(len);
+
+  printf("String s: %s\n", s);
+
+  free(s);
+
+  return 0;
+}
