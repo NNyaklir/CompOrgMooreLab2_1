a1183b23bc7ab441f6eb97b23fa0c2c59e3f14f7
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-02-20 09:42:09 -0700

    minor bugfix

--- ArrayList.c
+++ ArrayList.c
@@ -7,7 +7,7 @@ typedef struct{
 } ArrayList_t;
 
 //1
-ArrayList_t *AL_init(void){
+ArrayList_t* AL_init(void){
     ArrayList_t *list = malloc(sizeof(ArrayList_t));// accrue space
     if (list==NULL){
         return NULL; //if empty show its empty
