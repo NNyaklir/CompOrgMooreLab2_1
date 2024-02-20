09b19c208c2354d6cd76259b82d7c9e0386889c7
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-02-20 09:20:43 -0700

    added initilization

--- ArrayList.c
+++ ArrayList.c
@@ -4,14 +4,19 @@ typedef struct{
     void **array;
     size_t size;
     size_t capacity;
-
-
-
 } ArrayList_t;
 
 //1
 ArrayList_t *AL_init(void){
+    ArrayList_t *list = malloc(sizeof(ArrayList_t));// accrue space
+    if (list==NULL){
+        return NULL; //if empty show its empty
+    }
 
-    
+    list->array = NULL;
+    list->size = 0;
+    list->capacity = 0; initilize at zero
+
+    return list; //returns pointer of list
 }
 
