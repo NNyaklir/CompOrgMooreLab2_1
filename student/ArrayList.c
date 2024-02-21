344a5c368e18e7a1631554bf0b56089f0aa91aa4
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-02-20 20:30:11 -0700

    added free method

--- student/ArrayList.c
+++ student/ArrayList.c
@@ -22,3 +22,20 @@ ArrayList_t *AL_init(void){
     return list; //returns pointer of list
 }
 
+//2
+int AL_free(ArrayList_t *AL, int (*delete_data)(void *)){
+    if (AL==NULL){
+        return 1; //if null failure
+    }
+    for (size_t i=0; i<AL->size; i++){
+        if (delete_data!=NULL){
+            delete_data(AL->array[i]);
+        }
+        free(AL->array[i]);//frees individual data
+    }
+    free(AL->array);
+    free(AL);
+
+    return 0; //success
+}
+
